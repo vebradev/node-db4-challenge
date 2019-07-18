2a5488d1ab95f20a99dfd608afa01f122331cf9f
vebradev <justinas@vebra.dev>
2019-07-19 01:19:30 +0300

    Finish db helpers & endpoints for MVP

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -8,13 +8,22 @@ module.exports = {
 }
 
 function getRecipes() {
-  return "recipes";
+  return db("recipes");
 }
 
 function getShoppingList(recipe_id) {
-
+  return db
+    .select("recipes.name", "ingredients.name", "combined_recipe.quantity")
+    .from("combined_recipe")
+    .leftJoin("recipes", "combined_recipe.recipe_id", "recipes.id")
+    .leftJoin("ingredients", "combined_recipe.ingredient_id", "ingredients.id")
+    .where("combined_recipe.recipe_id", recipe_id);
 }
 
 function getInstructions(recipe_id) {
-  
+  return db
+    .select("steps.step_number", "steps.step")
+    .from("recipes")
+    .leftJoin("steps", "recipes.id", "steps.recipe_id")
+    .where("recipes.id", recipe_id);
 }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,14 +1,49 @@
-const express = require('express');
-const Recipes = require('./recipes-model');
+const express = require("express");
+const Recipes = require("./recipes-model");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.send("test");
+    const recipes = await Recipes.getRecipes();
+    res.json(recipes);
   } catch (error) {
     res.status(500).json({
-      error: 'Failed to load recipes.'
+      error: "Failed to load recipes."
+    });
+  }
+});
+
+router.get("/:id/shoppingList", async (req, res) => {
+  try {
+    const shoppingList = await Recipes.getShoppingList(req.params.id);
+    if (shoppingList.length) {
+      res.json(shoppingList);
+    } else {
+      res.json({
+        error: "Such recipe does not exist."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `Failed to load shopping list.`
+    });
+  }
+});
+
+router.get("/:id/instructions", async (req, res) => {
+  try {
+    const instructions = await Recipes.getInstructions(req.params.id);
+    if (instructions.length) {
+      res.json(instructions);
+    } else {
+      res.json({
+        error: "Such recipe does not exist."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `Failed to load steps for recipe.`
     });
   }
 });
